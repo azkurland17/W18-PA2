e31d6a9eaa63cacfa0c9d92ce835be607fc8fd3d
Alexandra Z Kurland <azkurlan@ieng6-201.ucsd.edu>
2018-02-06 11:40:25 -0800

    pq

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -13,12 +13,21 @@
 #include <queue>
 #include <vector>
 
+template<class T>
+bool compare(std::pair<float,Vertex<T>*> p1, std::pair<float,Vertex<T> *> p2 ){
+    return p1.first < p2.first;
+}
+
+
 template <class T>
 float dijkstra(Graph<T>&g, T src) {
+//initialize the vertices
+//each vertex dis already max
+std::priority_queue< std::pair<float,Vertex<T>*> , std::vector<std::pair<float,Vertex<T>*>>,
+ bool(*)(std::pair<float,Vertex<T>*>,std::pair<float, Vertex<T>*>)> pq(compare);
   
   
-  
-  return 0.0;
+return 0.0;
 }
 
 #endif
