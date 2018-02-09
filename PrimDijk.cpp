48a6f4dec3985ceb0c9d6f023035c9a542e31ab0
Alexandra Z Kurland <azkurlan@ieng6-202.ucsd.edu>
2018-02-08 23:24:20 -0800

    adding weights

--- PrimDijk.cpp
+++ PrimDijk.cpp
@@ -37,24 +37,32 @@ std::priority_queue< std::pair<float,Vertex<T>*> , std::vector<std::pair<float,V
  	std::pair<float, Vertex<T>*> popped = pq.top();
 	pq.pop();
 	Vertex<T>* curr = popped.second;
+	if(!curr->visited){
 	std::cout<< "popped node: "<<curr->id<<std::endl; //delete
+	}
 	if(!curr->visited){ //changed
-		float weight = g.get_weight(curr->id,curr->prev);
+		float weight = g.get_weight(curr->prev, curr->id);
 		spt = spt + weight;
 		std::cout<<"spt +" <<weight<< std::endl; //delete
 		if(!curr->visited){ //not visited
 	 		curr->visited = true;
 			//for each neighbor of curr
 			Vertex<T> * n;
-			float total_dist = 0;
 			for(auto itr = curr->edges.begin(); itr!= curr->edges.end(); itr++ ){
 				auto i = g.vertices.find(*itr);
 				n = i->second; //n is the neighbor of curr
-				float total_dist = c*(curr->distance) + g.get_weight(curr->id, n->id); //dist in curr + weight of new edge 
+				//n->distance = g.get_weight(curr->id, n->id);
+				float total_dist = c*curr->distance + g.get_weight(curr->id, n->id); //dist in curr + weight of new edge 
+				std::cout<< "total dist= " << total_dist<< std::endl;	
 				if(total_dist < n->distance){
 					n->prev = curr->id;
 					n->distance = total_dist;
-	 				std::pair<float, Vertex<T>*>pair (total_dist, n); //make pair to push to pq
+					
+	 				std::cout<<"WEIGHT B4 : " << g.get_weight(n->id, curr->id)<<std::endl;
+					g.set_weight(curr->id, n->id, total_dist);
+					//g.set_weight(n->id, curr->id, total_dist);
+	 				std::cout<<"WEIGHT AFTER : " << g.get_weight(curr->id, n->id)<<std::endl;
+					std::pair<float, Vertex<T>*>pair (total_dist, n); //make pair to push to pq
 					pq.push(pair);
 					std::cout<<"pushing pair("<<total_dist<<","<<n->id<<")"<<std::endl;
 				}
