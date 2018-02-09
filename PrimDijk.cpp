110d6569f004ad94c9b450b2c1ab240a7abcdd62
(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧* <heathlydallows@Jennifer.local>
2018-02-08 21:47:39 -0800

    primdijk

--- PrimDijk.cpp
+++ PrimDijk.cpp
@@ -13,9 +13,56 @@
 #include <queue>
 #include <vector>
 
+template<class T>
+bool compare(std::pair<float,Vertex<T>*> p1, std::pair<float,Vertex<T> *> p2 ){
+    return p1.first > p2.first;
+}
+
+
 template <class T>
 float primdijk(Graph<T>& g, T src, float c) {
-  // TODO: implement PrimDijk
-  return 0.0;
+float spt = 0;
+//initialize the vertices
+g.clear_distances(); //set dist to max 
+
+std::priority_queue< std::pair<float,Vertex<T>*> , std::vector<std::pair<float,Vertex<T>*>>,
+ bool(*)(std::pair<float,Vertex<T>*>,std::pair<float, Vertex<T>*>)> pq(compare);
+ 
+ auto it = g.vertices.find(src);
+ Vertex<T> * start = it->second; //set starting vertex to source given by src
+ start->distance = 0; 
+ std::pair<float, Vertex<T>*>p (0, start); //make pair to push on pq
+ pq.push(p);
+ while(!pq.empty()){
+ 	std::pair<float, Vertex<T>*> popped = pq.top();
+	pq.pop();
+	Vertex<T>* curr = popped.second;
+	std::cout<< "popped node: "<<curr->id<<std::endl; //delete
+	if(!curr->visited){ //changed
+		float weight = g.get_weight(curr->id,curr->prev);
+		spt = spt + weight;
+		std::cout<<"spt +" <<weight<< std::endl; //delete
+		if(!curr->visited){ //not visited
+	 		curr->visited = true;
+			//for each neighbor of curr
+			Vertex<T> * n;
+			float total_dist = 0;
+			for(auto itr = curr->edges.begin(); itr!= curr->edges.end(); itr++ ){
+				auto i = g.vertices.find(*itr);
+				n = i->second; //n is the neighbor of curr
+				float total_dist = c*(curr->distance) + g.get_weight(curr->id, n->id); //dist in curr + weight of new edge 
+				if(total_dist < n->distance){
+					n->prev = curr->id;
+					n->distance = total_dist;
+	 				std::pair<float, Vertex<T>*>pair (total_dist, n); //make pair to push to pq
+					pq.push(pair);
+					std::cout<<"pushing pair("<<total_dist<<","<<n->id<<")"<<std::endl;
+				}
+			}
+		}	
+	}
+ }
+return spt;
+
 }
 #endif
